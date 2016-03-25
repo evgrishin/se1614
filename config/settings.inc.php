1657aecc633a3f7b49012e6311f6171fe8eb22e7
Evgeny Grishin <e.v.grishin@yandex.ru>
2016-03-25 13:39:00 +0200

    commit

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -10,7 +10,7 @@ define('_PS_CACHE_ENABLED_', '0');
 define('_COOKIE_KEY_', 'xE0QPVQmf697FRx9TSX1hbpPcfo9Zv6BpSHUYswebnhEVIqFGjtcPZGm');
 define('_COOKIE_IV_', 'aZ879Koh');
 define('_PS_CREATION_DATE_', '2016-02-05');
-if (!defined('_PS_VERSION_'))
+if (!defined('_PS_VERSION_')) 
 	define('_PS_VERSION_', '1.6.1.4');
 define('_RIJNDAEL_KEY_', '7QzsoYWtUMIZ12iyYaMLNbPcBtz4tHdD');
 define('_RIJNDAEL_IV_', 'qGK8h03dK9t+rQp3ZphD4g==');
